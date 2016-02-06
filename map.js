1873a5edd07e935ec1592dda3c639a47562f89a2
Joshua Singer <josh.j.singer@gmail.com>
2016-02-06 15:50:21 -0800

    testing parse

--- map.js
+++ map.js
@@ -21,9 +21,9 @@ exports.addressToCoordinates = function(address) {
     address = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address.split(" ").join("+") + "&key=AIzaSyChCIMnLJFcujELe5FdvrAKuYCMG9IJJDc";
     request(address, function(error, response, body){
        if (!error && response.statusCode == 200) {
-           console.log(body);
-           console.log("Lat: " + JSON.parse(body[0].results[0]));
-           console.log("Lon: " + JSON.parse(body[0].results[0]));
+           //console.log(body);
+           //console.log("Lat: " + JSON.parse(body[0].results[0]));
+           //console.log("Lon: " + JSON.parse(body[0].results[0]));
        } else {
            console.log(error);
        }
