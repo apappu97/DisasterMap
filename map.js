5623cee1b1348f87c9d9fd00b0e1f8f4cac3a13c
Joshua Singer <josh.j.singer@gmail.com>
2016-02-06 15:28:35 -0800

    testing map code

--- map.js
+++ map.js
@@ -21,7 +21,7 @@ exports.addressToCoordinates = function(address) {
     address = "https://maps.googleapis.com/maps/api/geocode/json?" + address.split(" ").join("+") + "&key=AIzaSyB1zP1yILbaQTz49V7BnkUIqMHXTmwtxp0";
     request(address, function(error, response, body){
        if (!error && response.statusCode == 200) {
-           console.log("Lat: " + body.results.geometry.location.lat);
+           console.log("Lat: " + body.results[0].geometry.location.lat);
            console.log("Lon: " + body.results.geometry.location.lng);
        }
     });
