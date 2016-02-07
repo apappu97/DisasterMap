70b8dade59a9639fb7afc1b8e017147ecb256a36
Joshua Singer <josh.j.singer@gmail.com>
2016-02-06 21:02:18 -0800

    tys number

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.listen(8080, function() {
     twilio.messages.create({
         body: "This is a message from your local nonprofit. Please send us your address and needs in the following format." + os.EOL +
         "ADDRESS:" + os.EOL + "STATUS:",
-        to: "+12622717436",
+        to: "+12102683553",
         from: "+12108800132"
     }, function(err, message) {
         process.stdout.write(message.sid);
