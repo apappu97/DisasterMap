870218e0245a4a00a459301b8336b48de02aad34
Aneesh Pappu <apappu97@gmail.com>
2016-02-09 12:59:58 -0800

    moved index.html into public

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,8 +7,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('../../routes/index');
+var users = require('../../routes/users');
 var os = require('os');
 var S = require('string');
 var map = require('./map.js');
