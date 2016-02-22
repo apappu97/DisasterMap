6b32f5be3e41dbd6eeaa769daea878bb77d9d949
Joshua Singer <josh.j.singer@gmail.com>
2016-02-22 12:23:29 -0800

    deleted code

--- app.js
+++ app.js
@@ -127,14 +127,7 @@ app.post('/phone', function(req, res) {
         body:"This is a message from your local nonprofit. Please send us your address and needs in the following format."+ os.EOL +
         "ADDRESS:"+ os.EOL + "STATUS:"
     }, function(error, message) {
-        // The HTTP request to Twilio will run asynchronously. This callback
-        // function will be called when a response is received from Twilio
-        // The "error" variable will contain error information, if any.
-        // If the request was successful, this value will be "falsy"
         if (!error) {
-            // The second argument to the callback will contain the information
-            // sent back by Twilio for the request. In this case, it is the
-            // information about the text messsage you just sent:
             console.log('Success! The SID for this SMS message is:');
             console.log(message.sid);
 
